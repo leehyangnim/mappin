441c51df7518de9fbcc5e36bea68e73bde53676d
Hyang Im Lyn Lee <leehyangnim@hotmail.com>
2016-08-25 13:30:11 -0700

    aws updated

--- config/initializers/setup_fog.rb
+++ config/initializers/setup_fog.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region:                'us-west-2',                  # oregon
   }
-  config.fog_directory  = 'lyn-awesome-answers'                          # required
+  config.fog_directory  = 'mappinyvr'                          # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
